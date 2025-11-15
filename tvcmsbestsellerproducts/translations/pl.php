7b713f9a17e0ea84b3500aea4e96bc4df6aed45e
AZADAGROUP1 <michalmotykapl@gmail.com>
2025-11-15 22:18:35 +0100

    nic

---
+++ tvcmsbestsellerproducts/translations/pl.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>tvcmsbestsellerproducts_8d6c2b381dd0d8f1c64441ff12ed706a'] = 'ThemeVolty - Bestsellery';
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>tvcmsbestsellerproducts_cbdfd85ea410ca73a6af1c31c8dac19e'] = 'Wyświetla bestsellery na stronie głównej. (lub na stronie klienta.)';
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>tvcmsbestsellerproducts_077bd3a4f045db0eae3560ce0f149974'] = 'Ostrzeżenie: wszystkie dane zapisane w Twojej bazie danych zostaną usunięte! Czy na pewno chcesz odinstalować ten moduł?';
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>display_home-data_a9632d0c3014506b39a9685123aff62c'] = 'Baner z ofertą bestsellerów';
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>display_home-data_632b7e0d21c55858f4300ae6dc00a869'] = 'Wszystkie bestsellery';
+$_MODULE['<{tvcmsbestsellerproducts}prestashop>display_side_product_632b7e0d21c55858f4300ae6dc00a869'] = 'Wszystkie bestsellery';
